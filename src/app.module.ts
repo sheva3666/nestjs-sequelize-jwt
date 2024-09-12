e427d32c59e22c9700aa448ef33a8c2ee2106a2f
sheva3666 <ivan.shevchenko@sap.com>
2024-09-12 16:37:12 +0200

    made process env working

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,16 +2,17 @@ import { Module } from '@nestjs/common';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
-import dotenv from 'dotenv';
-
-dotenv.config();
+import * as dotenv from 'dotenv';
+import { ConfigModule } from '@nestjs/config';
+dotenv.config({ path: process.cwd() + '../.env' });
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     SequelizeModule.forRoot({
       dialect: 'mysql',
       host: process.env.HOST,
-      port: 8080,
+      port: 3306,
       username: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { User } from './user.model/user.model';
 
 @Module({
+  imports: [SequelizeModule.forFeature([User])],
   providers: [UsersService],
   controllers: [UsersController],
+  exports: [UsersService],
 })
 export class UsersModule {}
